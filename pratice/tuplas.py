a1202a434644fc303a4246e24ca6d2ba9b4ce54b
SergioFelzener <felzenerdj@gmail.com>
2022-06-28 20:12:41 -0400

    atualizando 2

---
+++ pratice/tuplas.py
@@ -0,0 +1,10 @@
+#TUPLAS (parecidas com lista / não pode adicinar mais elementos nem remover)
+
+colors = ("red", "green", "blue", "purple", "white", "black", "*e*")
+print(colors)
+
+tupla = colors.count("*e*")
+print (tupla)
+
+for color in colors:
+    print(color)
