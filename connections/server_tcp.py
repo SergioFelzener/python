2c5402cde610b47ea802d347eb96fd8f1892625e
SergioFelzener <felzenerdj@gmail.com>
2022-08-13 19:59:34 -0400

    finalizando servidor CHAT tcp

--- connections/server_tcp.py
+++ connections/server_tcp.py
@@ -1,26 +1,34 @@
-import socket
+import socket  
+# Criando servidor para escutar em uma porta e receber conexão tcp pelo NetCat
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# file = open("output.txt", "w")
 try:
-    server.bind(('0.0.0.0', 8883)) # porta que aguarda o clinet se conectar 0.0.0.0 -> dinamico consigo conectar de qq ip
-    server.listen(5)
+    server.bind(('0.0.0.0', 7773)) # porta que aguarda o clinet se conectar 0.0.0.0 -> dinamico consigo conectar de qq ip
+    server.listen(5) #usando metodo para poder escutar - parametro - numero de conexoes
     print("Connection OK ...")
     print("Listening........")
-    
-    client_socket, address = server.accept()
+    # atribuindo tupla em duas variáveis 
+    client_socket, address = server.accept() #retorna tupla 1 elemento socket / segundo elemento outra tupla com ip e porta
     client_socket.send(b"Conected success...\nSend Message :")
     
+    
+    # chat
     while True:
         print("Connection from ip :" + address[0] + "\nWaiting...")
         data = client_socket.recv(1024).decode()
         if data == "senhasecreta\n":
             print(data + "\n")
-            client_socket.send(b"Mensagem Secreta DONE !!!!\n")
+            # print("passou aqui \n")
+            client_socket.send(b"Accept Code DONE.\nConnection CLOSE!")
+            print(server.close()) # Nao esta fechando a conexao verificar.
         else:
             print("response :" + data + "\n")
             client_socket.send(input("Mensagem: ").encode())
     server.settimeout(30)
-    server.close()
 except Exception as error:
+    print("|DeBuG|Informação do sistema ->")
     print("Erro: ", error)
+    print("dentro da exception")
     server.close()
