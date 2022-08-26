68a898dd41479b53899f70316745fd1eb351ef62
SergioFelzener <felzenerdj@gmail.com>
2022-08-25 21:07:47 -0400

    finalizando API Python & Flask

--- flask/main.py
+++ flask/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)  ##Instanciando objeto da classe flask
@@ -12,6 +12,15 @@ class Post(db.Model):
     title = db.Column(db.String())
     author = db.Column(db.String())
     content = db.Column(db.String())
+    
+    def to_dict(self):
+        result = {}
+        for key in self.__mapper__.c.keys():
+            if getattr(self, key) is not None:
+                result[key] = getattr(self, key)
+            else:
+                result[key] = getattr(self, key)
+        return result
 
 
 try:
@@ -68,6 +77,71 @@ try:
                 
         return redirect(url_for("home"))
     
+    
+    # criando API 
+    
+    @app.route("/api/posts")
+    def api_list_posts():
+        try:
+            # name = request.args.get("name")
+            #return "<h1>Hello {}</h1>".format(name)
+            posts = Post.query.all() ## pegando todos os posts no db
+            # return render_template("index.html", posts=posts) ## passando templat tag post para front
+            # return jsonify({"password" : "teste"})
+            return jsonify([post.to_dict() for post in posts])
+        except Exception as error:
+            print("error" , error)
+            return jsonify({[]})
+            
+    @app.route("/api/post/add", methods=["PUT"])
+    def api_add_post():
+        try:
+            data = request.get_json()
+            post = Post(title=data["title"], content=data["content"], author=data["author"])
+            db.session.add(post)
+            db.session.commit()
+            return jsonify({"Success : " : True, "Cadastrado com sucesso" : post.id})
+        except Exception as error:
+            print("Error :", error)
+        
+        return jsonify({"Success : " : False})
+    
+    @app.route("/api/delete/post/<id>", methods=["DELETE"])
+    def api_delete_post(id):
+        try:
+            post = Post.query.get(id)
+            db.session.delete(post)
+            db.session.commit()
+            return jsonify({
+                            "Success : " : True,
+                            "Id Apagado" : post.id,
+                            "Cadastrado por" : post.author
+                           })
+        except Exception as error:
+            print("Error :", error)
+        
+        return jsonify({"Success : " : False})
+    
+    @app.route("/api/post/edit/<id>", methods=["PUT"])
+    def api_edit_post(id):
+        try:
+            post = Post.query.get(id)
+            data = request.get_json()
+            post.title = data["title"]
+            post.content = data["content"]
+            post.author = data["author"]
+            db.session.commit()
+            return jsonify({
+                            "Success : " : True, 
+                            "Editando ID": post.id,
+                            "Author" : post.author,
+                            "Título do Post" : post.title
+                           })
+        except Exception as error:
+            print("Error edit :", error)
+            
+            return jsonify({"Success : " : False})
+          
     db.create_all()
     app.run(debug=True)
 except Exception as error:
